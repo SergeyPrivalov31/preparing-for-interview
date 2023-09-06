24de1340a6d45e89bdbb64917b2d26ca9fc897e6
Sergey <68424586+SergeyPrivalov31@users.noreply.github.com>
2023-09-06 19:22:22 +0300

    create myMap with test, create mySome

--- myPolyfills/index.js
+++ myPolyfills/index.js
@@ -1,1 +1,34 @@
-module.exports = function () {};
+module.exports = function () {
+  //map
+  Array.prototype.myMap = function (callback) {
+    if (!(this instanceof Array || this instanceof String)) {
+      throw new TypeError("Array.prototype.myMap was called on wrong type");
+    }
+
+    if (typeof callback !== "function") {
+      throw new TypeError(`Array.prototype.myMap ${callback} is not a function`);
+    }
+
+    const result = [];
+    for (let i = 0; i < this.length; i++) {
+      result.push(callback(this[i], i, this));
+    }
+    return result;
+  };
+
+  //some
+  Array.prototype.mySome = function (callback) {
+    if (!this) {
+      throw new TypeError("Array.prototype.mySome called on null or undefined");
+    }
+    if (typeof callback !== "function") {
+      throw new TypeError(`Array.prototype.mySome ${callback} is not a function`);
+    }
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        return true;
+      }
+    }
+    return false;
+  };
+};
